977fed42e1639270792e1592bfac5f64e49a3118
EvanPiro <dev.evanpiro@gmail.com>
2022-12-24 14:48:59 -0500

    basic flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Dev shell sandbox";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:EvanPiro/nixpkgs";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [];
+      };
+      formatter = pkgs.alejandra;
+    });
+}
