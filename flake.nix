31eb23bcaa1542d364d6d2b73f62b35501025958
EvanPiro <dev.evanpiro@gmail.com>
2022-12-30 18:05:53 -0500

    refactor to fold

--- flake.nix
+++ flake.nix
@@ -11,19 +11,14 @@
   in {
     devShells = nixpkgs.lib.pipe systems [
       (builtins.map (system: {
-        name = system;
-        value = {
-          default = let
-            pkgs = nixpkgs.legacyPackages.${system}.pkgs;
-          in
-            pkgs.mkShell {
-              packages = with pkgs; [
-                postgresql
-              ];
-            };
-        };
+        ${system}.default = with nixpkgs.legacyPackages.${system}.pkgs;
+          pkgs.mkShell {
+            packages = with pkgs; [
+              postgresql
+            ];
+          };
       }))
-      builtins.listToAttrs
+      (builtins.foldl' nixpkgs.lib.mergeAttrs {})
     ];
 
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
