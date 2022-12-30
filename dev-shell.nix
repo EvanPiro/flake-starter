fa40539eb774414637e92102a8c42d5f0cc96ed0
EvanPiro <dev.evanpiro@gmail.com>
2022-12-30 17:46:18 -0500

    remove dev shell function

--- dev-shell.nix
+++
@@ -1,9 +0,0 @@
-{
-  system,
-  nixpkgs,
-}: let
-  pkgs = nixpkgs.legacyPackages.${system}.pkgs;
-in
-  pkgs.mkShell {
-    packages = with pkgs; [postgresql];
-  }
--- flake.nix
+++ flake.nix
@@ -7,15 +7,25 @@
     self,
     nixpkgs,
   }: let
-    mkDevShell = import ./dev-shell.nix;
     systems = ["x86_64-linux" "x86_64-darwin"];
   in {
-    devShells = {
-      x86_64-linux.default = mkDevShell {
-        inherit nixpkgs;
-        system = "x86_64-linux";
-      };
-    };
+    devShells = nixpkgs.lib.pipe systems [
+      (builtins.map (system: {
+        name = system;
+        value = {
+          default = let
+            pkgs = nixpkgs.legacyPackages.${system}.pkgs;
+          in
+            pkgs.mkShell {
+              packages = with pkgs; [
+                postgresql
+              ];
+            };
+        };
+      }))
+      builtins.listToAttrs
+    ];
+
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
   };
 }
