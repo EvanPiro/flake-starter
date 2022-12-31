b636011dacbdc2351bd0008a259093ab4feb9d59
EvanPiro <dev.evanpiro@gmail.com>
2022-12-31 00:50:17 -0500

    add shell

--- flake.nix
+++ flake.nix
@@ -9,18 +9,14 @@
   }: let
     # Add supported systems
     systems = ["x86_64-darwin" "x86_64-linux"];
+    mkDevShell = import ./shell.nix;
   in
     nixpkgs.lib.pipe systems [
       (builtins.map (system: let
         pkgs = nixpkgs.legacyPackages.${system}.pkgs;
       in {
         # Add set conforming to flake schema here:
-        devShells.${system}.default = pkgs.mkShell {
-          packages = with pkgs; [
-            # Add packages from nixpkgs like the following:
-            # postgresql
-          ];
-        };
+        devShells.${system}.default = mkDevShell pkgs;
         formatter.${system} = pkgs.alejandra;
       }))
 
