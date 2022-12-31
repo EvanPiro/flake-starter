b3f10a881ea011db4d979102196fc0aa72b4581e
EvanPiro <dev.evanpiro@gmail.com>
2022-12-31 01:37:09 -0500

    add pg shellhooks

--- shell.nix
+++ shell.nix
@@ -1,10 +1,12 @@
-pkgs: pkgs.mkShell {
+pkgs:
+pkgs.mkShell {
   buildInputs = with pkgs; [
     postgresql
   ];
 
   shellHook = ''
-    pg_ctl -D ./data -l logfile -o "-k /tmp" start
+    initdb -D ./data;
+    pg_ctl -D ./data -l logfile -o "-k /tmp" start;
   '';
 
   PGHOST = "/tmp";
